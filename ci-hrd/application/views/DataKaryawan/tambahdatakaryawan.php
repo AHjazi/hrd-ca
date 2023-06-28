a6b7514626865723824879288e3f3a3dc4772987
AHjazi <ahmdhjzi06@gmail.com>
2023-06-28 13:22:38 +0800

    Tampilan, form input, dan detail

--- ci-hrd/application/views/DataKaryawan/tambahdatakaryawan.php
+++ ci-hrd/application/views/DataKaryawan/tambahdatakaryawan.php
@@ -3,7 +3,7 @@
         <div class="row column_title">
             <div class="col-md-12">
                 <div class="page_title">
-                    <h2>STATUS KARYAWAN</h2>
+                    <h2>TAMBAH DATA KARYAWAN</h2>
                 </div>
             </div>
         </div>
@@ -18,6 +18,58 @@
                         placeholder="">
                 </div>
                 <div class="form-group">
+                    <label for="formGroupExampleInput2">Nama</label>
+                    <input type="text" class="form-control col-md-6" name="" id="formGroupExampleInput2" placeholder="">
+                </div>
+                <div class="form-group">
+                    <label for="formGroupExampleInput2">Tanggal Masuk</label>
+                    <input type="date" class="form-control col-md-6" name="" id="formGroupExampleInput2" placeholder="">
+                </div>
+                <div class="form-group">
+                    <label for="formGroupExampleInput2">Masa Kerja</label>
+                    <input type="date" class="form-control col-md-6" name="" id="formGroupExampleInput2" placeholder="">
+                </div>\
+                <div class="form-group">
+                    <label for="formGroupExampleInput2">Divisi</label>
+                    <input type="text" class="form-control col-md-6" name="" id="formGroupExampleInput2" placeholder="">
+                </div>
+                <div class="form-group">
+                    <label for="formGroupExampleInput2">Jabatan</label>
+                    <input type="text" class="form-control col-md-6" name="" id="formGroupExampleInput2" placeholder="">
+                </div>
+                <div class="form-group">
+                    <label for="formGroupExampleInput2">Status Terakhir</label>
+                    <input type="text" class="form-control col-md-6" name="" id="formGroupExampleInput2" placeholder="">
+                </div>
+                <div class="form-group">
+                    <label for="formGroupExampleInput2">Golongan</label>
+                    <input type="text" class="form-control col-md-6" name="" id="formGroupExampleInput2" placeholder="">
+                </div>
+                <div class="form-group">
+                    <label for="formGroupExampleInput2">BPJS Tenaga Kerja</label>
+                    <input type="text" class="form-control col-md-6" name="" id="formGroupExampleInput2" placeholder="">
+                </div>
+                <div class="form-group">
+                    <label for="formGroupExampleInput2">BPJS Kesehatan</label>
+                    <input type="text" class="form-control col-md-6" name="" id="formGroupExampleInput2" placeholder="">
+                </div>
+                <div class="form-group">
+                    <label for="formGroupExampleInput2">NPWP</label>
+                    <input type="text" class="form-control col-md-6" name="" id="formGroupExampleInput2" placeholder="">
+                </div>
+                <div class="form-group">
+                    <label for="formGroupExampleInput2">Status PTKP</label>
+                    <input type="text" class="form-control col-md-6" name="" id="formGroupExampleInput2" placeholder="">
+                </div>
+                <div class="form-group">
+                    <label for="formGroupExampleInput2">Alamat KTP</label>
+                    <input type="text" class="form-control col-md-6" name="" id="formGroupExampleInput2" placeholder="">
+                </div>
+                <div class="form-group">
+                    <label for="formGroupExampleInput2">Alamat Domisili</label>
+                    <input type="text" class="form-control col-md-6" name="" id="formGroupExampleInput2" placeholder="">
+                </div>
+                <div class="form-group">
                     <label for="formGroupExampleInput2">PKWT-Mulai</label>
                     <input type="date" class="form-control col-md-6" name="" id="formGroupExampleInput2" placeholder="">
                 </div>
@@ -30,23 +82,15 @@
                     <input type="date" class="form-control col-md-6" name="" id="formGroupExampleInput2" placeholder="">
                 </div>
                 <div class="form-group">
-                    <label for="formGroupExampleInput2">PKWT-Selesai</label>
+                    <label for="formGroupExampleInput2">PKWT2-Selesai</label>
                     <input type="date" class="form-control col-md-6" name="" id="formGroupExampleInput2" placeholder="">
                 </div>
                 <div class="form-group">
-                    <label for="formGroupExampleInput2">THT-Mulai</label>
+                    <label for="formGroupExampleInput2">THT</label>
                     <input type="date" class="form-control col-md-6" name="" id="formGroupExampleInput2" placeholder="">
                 </div>
                 <div class="form-group">
-                    <label for="formGroupExampleInput2">THT-Selesai</label>
-                    <input type="date" class="form-control col-md-6" name="" id="formGroupExampleInput2" placeholder="">
-                </div>
-                <div class="form-group">
-                    <label for="formGroupExampleInput2">THL-Mulai</label>
-                    <input type="date" class="form-control col-md-6" name="" id="formGroupExampleInput2" placeholder="">
-                </div>
-                <div class="form-group">
-                    <label for="formGroupExampleInput2">THL-Selesai</label>
+                    <label for="formGroupExampleInput2">THL</label>
                     <input type="date" class="form-control col-md-6" name="" id="formGroupExampleInput2" placeholder="">
                 </div>
                 <div class="form-group">
@@ -56,12 +100,19 @@
                 <div class="form-group">
                     <label for="formGroupExampleInput2">Percobaan-Selesai</label>
                     <input type="date" class="form-control col-md-6" name="" id="formGroupExampleInput2" placeholder="">
-                    </select>
+                </div>
+                <div class="form-group">
+                    <label for="formGroupExampleInput2">Karyawan Pribadi</label>
+                    <input type="text" class="form-control col-md-6" name="" id="formGroupExampleInput2" placeholder="">
+                </div>
+                <div class="form-group">
+                    <label for="formGroupExampleInput2">Keluarga</label>
+                    <input type="text" class="form-control col-md-6" name="" id="formGroupExampleInput2" placeholder="">
                 </div>
                 <br>
                 <td>
-                    <a href="<?php echo base_url('DataKaryawan/tambahdatakeluarga')?>"><button
-                            class="btn btn-danger btn-"><i class="fa fa-arrow-circle-left">
+                    <a href="<?php echo base_url('DataKaryawan/karyawan')?>"><button class="btn btn-danger btn-"><i
+                                class="fa fa-arrow-circle-left">
                             </i> Back</button></a>
                     <a href="<?php echo base_url('DataKaryawan/pribadi')?>"><button class="btn btn-primary btn-">Simpan
                             <i class="fa fa-save">
